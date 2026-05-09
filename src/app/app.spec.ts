d2354936fa1e1eae8a89e5994d5d5be3da00ce69
Paul Williams <paul58914080@gmail.com>
2026-05-09 12:03:25 +0530

    test: update loading of translate

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,30 @@
 import { TestBed } from "@angular/core/testing";
 import { App } from "./app";
+import {
+  TranslateLoader,
+  provideTranslateLoader,
+  provideTranslateService,
+} from "@ngx-translate/core";
+import { of } from "rxjs";
+import en from "../../public/assets/i18n/en.json";
+
+class TestTranslateLoader extends TranslateLoader {
+  getTranslation() {
+    return of(en);
+  }
+}
 
 describe("App", () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideTranslateService({
+          loader: provideTranslateLoader(TestTranslateLoader),
+          fallbackLang: "en",
+          lang: "en",
+        }),
+      ],
     }).compileComponents();
   });
 
@@ -16,6 +36,7 @@ describe("App", () => {
 
   it("should render title", async () => {
     const fixture = TestBed.createComponent(App);
+    fixture.detectChanges();
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector("h1")?.textContent).toContain(
